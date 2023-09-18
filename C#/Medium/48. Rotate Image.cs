7c9eb8de434462b83dc5089c4d1ab00308cdd2bf
Nirav Patel <64038962+Niravanaa@users.noreply.github.com>
2023-09-18 18:13:25 -0400

    Solved 48. Rotate Image
        Transpose the Matrix: Think of this as flipping the matrix along its main diagonal, like folding a piece of paper from its top-left corner to its bottom-right corner. This is done in two nested loops:
            The outer loop (for (int i = 0; i < n; i++)) iterates over the rows of the matrix.
            The inner loop (for (int j = i; j < n; j++)) iterates over the columns of the matrix, starting from the diagonal element (i).
            Inside the inner loop, we swap the values at matrix[i][j] and matrix[j][i]. This effectively reflects elements across the main diagonal, achieving the transpose operation.
    
        Reverse Each Row: Now, imagine each row as a horizontal line of elements. We want to reverse the order of elements in each row, which is similar to flipping each row left to right. Again, this is done using two nested loops:
            The outer loop (for (int i = 0; i < n; i++)) iterates over each row.
            The inner loop (for (int j = 0; j < n / 2; j++)) goes halfway across each row.
            Inside the inner loop, we swap the values at matrix[i][j] and matrix[i][n - 1 - j]. This reverses the elements within each row.

---
+++ C#/Medium/48. Rotate Image.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int n = matrix.Length;
+        
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                // Swap matrix[i][j] and matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                // Swap matrix[i][j] and matrix[i][n - 1 - j]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
