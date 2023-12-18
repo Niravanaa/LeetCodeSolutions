31c3ee3e9133c4573268fd1577066da0d328d73d
Nirav Patel <niravp0703@gmail.com>
2023-12-18 10:16:42 -0500

    Solved 1913. Maximum Product Difference Between Two Pairs
        Input: The function MaxProductDifference takes in an array of integers called nums.
    
        Sorting: The code begins by sorting the nums array in ascending order using Array.Sort(nums).
    
        Finding Maximum Product Difference:
    
            It then calculates the maximum product difference by performing the following:
    
            Step 1: Multiplying the last two numbers in the sorted array (nums[nums.Length - 1] * nums[nums.Length - 2]). These are the two largest numbers in the array.
    
            Step 2: Subtracting the product of the first two numbers in the sorted array (nums[0] * nums[1]). These are the two smallest numbers in the array.
    
        Return Value: The function returns the result of the calculated maximum product difference.
    
        Time Complexity: The time complexity of this code is primarily determined by the sorting operation, which is O(n log n), where 'n' is the number of elements in the nums array. The rest of the operations are constant time, so they do not significantly impact the overall complexity.
    
        Space Complexity: The code uses only a constant amount of extra space for variable storage, so the space complexity is O(1), meaning it does not increase with the size of the input array.

---
+++ C#/Easy/1913. Maximum Product Difference Between Two Pairs.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int MaxProductDifference(int[] nums) {
+        Array.Sort(nums);
+
+        return nums[nums.Length - 1] * nums[nums.Length - 2] - ( nums[0] * nums[1] );
+    }
+}
