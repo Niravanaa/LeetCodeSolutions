9a54cef7e79a7fe5ca2c79458c3346576a8a9a05
Nirav Patel <niravp0703@gmail.com>
2024-03-11 17:55:03 -0400

    Solved 206. Reverse Linked List
        We start with three pointers: prev, current, and next. prev will keep track of the previous node, current will keep track of the current node, and next will keep track of the next node.
    
        We initialize prev to null because there's no previous node to the head node initially. current is set to the head, which is the start of the linked list.
    
        We start a while loop that runs as long as current is not null, meaning there are still nodes to process.
    
        Inside the loop, we first store the next node of the current node. This is crucial because once we change the current.next pointer, we'll lose track of the rest of the list.
    
        Then, we change the current.next pointer to point to the prev node. This effectively reverses the link of the current node to its previous node.
    
        Next, we update prev to be the current node, as we'll move to the next node in the next iteration.
    
        Finally, we update current to be the next node, allowing us to move forward in the list.
    
        Once the loop finishes, prev will be pointing to the last node of the original list, which is now the first node of the reversed list. So, we return prev.
    
    The time complexity of this algorithm is O(n), where n is the number of nodes in the linked list, because we traverse the entire list once. The space complexity is O(1), because we only use a constant amount of extra space regardless of the size of the input list.

---
+++ Java/Easy/206. Reverse Linked List.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null)
+        {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
