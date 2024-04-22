c31efa70850a1cb9bce1cb0b4396427d047abc75
Nirav Patel <niravp0703@gmail.com>
2024-04-22 12:00:00 -0400

    2073. Time Needed to Buy Tickets

---
+++ Python/Easy/2073. Time Needed to Buy Tickets.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def timeRequiredToBuy(self, tickets, k):
+        """
+        :type tickets: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return sum(min(x, tickets[k] if i <= k else tickets[k]-1) for i, x in enumerate(tickets))
