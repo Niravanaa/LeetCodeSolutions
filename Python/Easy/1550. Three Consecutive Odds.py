447f524470f748e41e6c6088c75aa2ae7b17e7ad
Nirav Patel <niravp0703@gmail.com>
2024-04-28 12:00:00 -0400

    1550. Three Consecutive Odds

---
+++ Python/Easy/1550. Three Consecutive Odds.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for x in arr:
+            count = count+1 if x%2 else 0
+            if count == 3:
+                return True
+        return False
