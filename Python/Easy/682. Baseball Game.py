f6999ca5d08d0fc726f8cf69ca30c09900fa518c
Nirav Patel <niravp0703@gmail.com>
2024-05-28 18:40:19 -0400

    Solved 682. Baseball Game
        Initialize an empty list: Start with an empty list called scores to keep track of all the valid scores throughout the game.
    
        Iterate over each operation: Go through each item in the list of operations one by one.
    
        Process each operation:
            If the operation is a number: Convert it to an integer and add it to the scores list. This represents recording a new score.
            If the operation is '+': Calculate the sum of the last two scores in the scores list and add this sum to the scores list. This represents recording a new score that is the sum of the previous two scores.
            If the operation is 'D': Double the last score in the scores list and add this new value to the scores list. This represents recording a new score that is double the previous score.
            If the operation is 'C': Remove the last score from the scores list. This represents invalidating the previous score.
    
        Sum up all the scores: After processing all operations, calculate the total sum of all the scores in the scores list. This sum is the final result.
    
    Time and Space Complexity
    
        Time Complexity:
            Each operation is processed once, so if there are n operations, the time complexity is O(n).
    
        Space Complexity:
            We use a list to store the scores. In the worst case, all operations could be valid scores, so the space required is proportional to the number of operations. Therefore, the space complexity is O(n).

---
+++ Python/Easy/682. Baseball Game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        scores = []
+
+        for op in operations:
+            if op == "+":
+                scores.append(scores[-1] + scores[-2])
+            elif op == 'D':
+                scores.append(2 * scores[-1])
+            elif op == 'C':
+                scores.pop()
+            else:
+                scores.append(int(op))
+        
+        return sum(scores)
