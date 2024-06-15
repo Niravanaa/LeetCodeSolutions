a1ec25eea9c7f2911d6926b1357d25dc309acc8f
Nirav Patel <niravp0703@gmail.com>
2024-06-14 21:25:15 -0400

    Revert "441. Arranging Coins"
    This reverts commit 2f090d291c1e15c4bb5e16ff981d0412575a41fa.

--- Python/Easy/441. Arranging Coins.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def arrangeCoins(self, n: int) -> int:
-        filled = 0
-        
-        while n >= filled + 1:
-            filled += 1
-            n -= filled
-            
-        return filled
